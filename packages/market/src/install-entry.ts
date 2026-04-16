0cfc457e6e4cc586a6d7357b9f8743f3de80369c
Bela Bohlender <bela@bohlender.email>
2026-04-17 01:20:31 +0200

    feat: add install entry point to @drawcall/market package

---
+++ packages/market/src/install-entry.ts
@@ -0,0 +1,8 @@
+export { createMarketClient } from './client.js'
+export type { MarketClient, MarketClientOptions } from './client.js'
+
+export { resolve, ResolutionError } from './resolve.js'
+export type { ResolvedAsset, ResolveResult } from './resolve.js'
+
+export { install } from './install.js'
+export type { InstallOptions } from './install.js'
